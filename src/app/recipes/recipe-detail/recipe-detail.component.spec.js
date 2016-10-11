6e9dbcb8798c5352174fbc1deec6d109d456035d
Jason Hodges <jason.hodges@clickfox.com>
2016-10-11 17:31:45 -0600

    minor update

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.spec.js
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var recipe_detail_component_1 = require('./recipe-detail.component');
+describe('Component: RecipeDetail', function () {
+    it('should create an instance', function () {
+        var component = new recipe_detail_component_1.RecipeDetailComponent();
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=recipe-detail.component.spec.js.map
